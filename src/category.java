d1f5638240cc8634a6392bc6fe2891fd6054f565
mashas <skr.mashulia@gmail.com>
2014-04-22 02:29:04 +0400

    i changed 2 class

---
+++ src/category.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+
+public class category {
+
+  public String nameClass;
+
+    public vector  myVehicle;
+
+  public void addClass() {
+  }
+
+  public void removeClass() {
+  }
+
+}
