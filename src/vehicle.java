74ff7a6724d568de23d0c5a4fa593fde63f22cbd
NadyaKuzenko <nadya.net206@mail.ru>
2014-04-23 19:46:34 +0300

    I've done 11

---
+++ src/vehicle.java
@@ -0,0 +1,31 @@
+import java.util.Vector;
+
+public class vehicle extends dateRange, dateRange {
+
+  public String Class;
+
+  public String make;
+
+  public String model;
+
+  public Integer odometer;
+
+  public String regNo;
+
+  public String colour;
+
+  public dateRange unvalidDates[];
+
+  public hireBooking hirings[];
+
+  public serviceBooking services[];
+
+    public vector  unvalidDates;
+    public vector  services;
+    public vector  hirings;
+    public vector  myColour;
+    public vector  myColour;
+    public vector  myCategory;
+    public vector  myComparison;
+
+}
