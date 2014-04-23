328d35425cf2336003a741580fbc00c35883c077
NadyaKuzenko <nadya.net206@mail.ru>
2014-04-23 19:43:11 +0300

    I've done 9

---
+++ src/serviceBooking.java
@@ -0,0 +1,11 @@
+import java.util.Vector;
+
+public class serviceBooking extends abstractBooking {
+
+  public Integer odometer;
+
+  public String serviceItems;
+
+    public vector  services;
+
+}
