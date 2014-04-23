59056aa57f3fbaa0dc0d519617ebef8cd4070116
NadyaKuzenko <nadya.net206@mail.ru>
2014-04-23 19:44:30 +0300

    I've done 10

---
+++ src/transaction.java
@@ -0,0 +1,14 @@
+import java.util.Vector;
+
+public class transaction {
+
+  public Integer amount;
+
+  public String reference;
+
+  public Integer transDate;
+
+    public vector  cost;
+    public vector  accountHistory;
+
+}
