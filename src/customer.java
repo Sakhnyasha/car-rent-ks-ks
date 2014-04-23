21965f178f23a18669c4babfc27bbe6511344bec
kotsovskyi <kotsovskyi@users.noreply.github.com>
2014-04-23 14:00:42 +0300

    My 5-th

---
+++ src/customer.java
@@ -0,0 +1,31 @@
+import java.util.Vector;
+
+public class customer {
+
+  public String name;
+
+  public String otherNmae;
+
+  public String title;
+
+  public String address;
+
+  public Integer workPhone;
+
+  public Integer homePhone;
+
+  public Integer fax;
+
+  public String email;
+
+  public hireBooking booking[];
+
+  public transaction accountHistory[];
+
+  public driver registeredDrivers[];
+
+    public vector  booking;
+    public vector  registeredDrivers;
+    public vector  accountHistory;
+
+}
