778d02750f7ae1c2f845881745b63f4ad8e7bc61
kotsovskyi <kotsovskyi@users.noreply.github.com>
2014-04-23 14:01:31 +0300

    My 6-th

---
+++ src/dateRange.java
@@ -0,0 +1,14 @@
+import java.util.Vector;
+
+public class dateRange {
+
+  public Integer dateFrom;
+
+  public Integer dateTo;
+
+  public String bookingType;
+
+    public Vector  bookedDateRange;
+    public Vector  unvalidDates;
+
+}
