57df51544ac958027786aaebea1c057abfdc1893
NadyaKuzenko <nadya.net206@mail.ru>
2014-04-23 19:41:44 +0300

    I've done 8

---
+++ src/hireBooking.java
@@ -0,0 +1,18 @@
+import java.util.Vector;
+
+public class hireBooking extends abstractBooking {
+
+  public customer customer;
+
+  public transaction cost;
+
+  public driver drivers[];
+
+  public Integer newAttr;
+
+    public vector  hirings;
+    public vector  booking;
+    public vector  cost;
+    public vector  drivers;
+
+}
