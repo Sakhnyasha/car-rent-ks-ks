4614255c3b5b83ace47bb9227f5de95e5efd6f2c
kotsovskyi <kotsovskyi@users.noreply.github.com>
2014-04-23 14:02:44 +0300

    My 7-th

---
+++ src/driver.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+
+public class driver {
+
+  public String firstNmae;
+
+  public String surName;
+
+  public Integer age;
+
+  public String licenceNo;
+
+    public vector  drivers;
+    public vector  registeredDrivers;
+
+}
